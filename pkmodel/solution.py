74351f73116a4fc0fb6fc135391b2f5528c56e94
Justin Leung <justinleung074170@gmail.com>
2022-10-19 15:30:06 +0100

    Added axes and figure titles to solution.py

--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -19,29 +19,6 @@ class Solution:
     def __init__(self, value=44):
         self.value = value
 
-def plot_drug(model,f):
-    """
-    Function that plots the concentration of the drug from the central
-    compartment and also the one/two peripheral compartments (up to 
-    the user's decision) over the time of the treatment.
-
-    Inputs
-    ------
-    model (dict): The model results that are to be plotted by the function 
-    
-    fig (figure): The figure that the data will be plotted on   
-
-    Outputs
-    -------
-    fig (figure): (same as fig input)
-
-    """
-
-    for comp in model['solution']:
-        f.axes[0].plot(model['t'],comp)
-    f.axes[1].plot(model['t'],model['dose'])
-    return f
-
 def solution(model_sol, *args):
     """ 
     Function that plots the results from the model and allows
@@ -63,14 +40,42 @@ def solution(model_sol, *args):
     Outputs
     -------
     """
-
-    f, (a0, a1) = plt.subplots(2, 1 , gridspec_kw={'height_ratios': [3, 1]})
-    for model in [model_sol, *args]:
-        plot_drug(model,f)
     
+    # create subplot for 
+    f, (a0,a1) = plt.subplots(2, 1 , gridspec_kw={'height_ratios': [3, 1]})
+    for model in [model_sol, *args]:    # calls plot_drug function
+        plot_drug(model, f)
+    
+     # adding visualisation elements
     f.legend()
-    a0.set_ylabel('drug mass [ng]')
+    f.suptitle('Drug amount in different systems')
+    a0.set_ylabel('drug amount in compartments [ng]')
+    a1.set_xlabel('drug dosage amount [ng]')
     a1.set_xlabel('time [h]')
     f.tight_layout()
 
+def plot_drug(model, f):
+    """
+    Function that plots the concentration of the drug from the central
+    compartment and also the one/two peripheral compartments (up to 
+    the user's decision) over the time of the treatment.
+
+    Inputs
+    ------
+    model (dict): The model results that are to be plotted by the function 
+    
+    fig (figure): The figure that the data will be plotted on   
+
+    Outputs
+    -------
+    fig (figure): (same as fig input)
+
+    """
+
+    # plots the drug concentration for each compartment 
+    for comp in model['solution']:
+        f.axes[0].plot(model['t'], comp)
+    f.axes[1].plot(model['t'], model['dose'])  # plots drug dosage
+
+    return f
 
