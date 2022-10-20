558996d8f90ed7b916b6f10f0c10eaeece6cd718
Kallista Angeloff <kallista.angeloff@env-res.ox.ac.uk>
2022-10-20 12:27:17 +0100

    Add test for successful completion to solver functions

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -11,6 +11,11 @@ def rhs_iv_one_compartment(t, y, model_input):
     
     Parameters
     ----------
+    t : array
+    `t` is an array containing the time over which to solve.
+    y : array
+    `y` is an array containing the quantities changing, e.g.
+    q_c, the amount of the drug in the main compartment.
     model_input : dict
         `model_input` is a dictionary containing the following:
         V_c : float
@@ -19,22 +24,15 @@ def rhs_iv_one_compartment(t, y, model_input):
             `CL` is the clearance rate in mL/hr of the main compartment.
         X : float
             `X` is the dose in ng of the drug.
-    dose : function
-        `dose` is a function that takes two floats, t and X, representing
-        the dosing protocol over time, and returns ... ?
-    t : array
-    X : float
-
     Return
     ----------
-    dqc_dt : float
+    dqc_dt : array
         `dqc_dt` is the rate of change of the drug in the main compartment
         over time, d(q_c)/dt.
     '''
     
-    q_c = y
+    q_c = y[0]
     dqc_dt = dose(t, model_input['X']) - q_c/model_input['V_c'] * model_input['CL']
-
     return dqc_dt
 
 def iv_one_compartment(t_eval, y0, model_input):
@@ -43,23 +41,28 @@ def iv_one_compartment(t_eval, y0, model_input):
     
     Parameters
     ----------
-    model_input : dict
-        `model_input` is a dictionary containing the following: 
-
-
-
     t_eval : array
         `t_eval` is an array containing the timespan over which to 
-        evaluate the differential equation.
-    y0 : array (n.b. one-element array here)
+        evaluate the differential equations.
+    y0 : array
         `y0` is an array containing the initial conditions for the 
-        differential equation.
+        differential equations.
+    model_input : dict
+        `model_input` is a dictionary containing the following: 
+        V_c : float
+            `V_c` is the volume in mL of the main compartment.
+        CL: float
+            `CL` is the clearance rate in mL/hr of the main compartment.
+        X : float
+            `X` is the dose in ng of the drug.
 
     Return
     ----------
-    sol_iv_one_compartment : 
-
-
+    sol_iv_one_compartment : bunch object OdeResult
+    Fields of interest: 
+        .t : array
+        .y : array
+    which give the time points and values of the solution at those points.
     '''
 
     sol_iv_one_compartment = scipy.integrate.solve_ivp(
@@ -67,7 +70,7 @@ def iv_one_compartment(t_eval, y0, model_input):
         t_span=[t_eval[0], t_eval[-1]],
         y0=y0, t_eval=t_eval
     )
-
+    print(sol_iv_one_compartment.message) # test
     return sol_iv_one_compartment
     
 # --- Two compartments --------------------------
@@ -124,7 +127,11 @@ def iv_two_compartments(t_eval, y0, model_input):
 
     Return
     ----------
-    sol_iv_two_compartments : 
+    sol_iv_two_compartments : bunch object OdeResult
+    Fields of interest: 
+        .t : array
+        .y : array
+    which give the time points and values of the solution at those points.
     '''
     
     sol_iv_two_compartments = scipy.integrate.solve_ivp(
@@ -132,7 +139,7 @@ def iv_two_compartments(t_eval, y0, model_input):
         t_span=[t_eval[0], t_eval[-1]],
         y0=y0, t_eval=t_eval
     )
-
+    print(sol_iv_two_compartments.message)
     return sol_iv_two_compartments
 
 # --- Subcutaneous ------------------------------
@@ -190,7 +197,11 @@ def subcutaneous(t_eval, y0, model_input):
 
     Return
     ----------
-    sol_subcutaneous : ###
+    sol_subcutaneous : bunch object OdeResult
+    Fields of interest: 
+        .t : array
+        .y : array
+    which give the time points and values of the solution at those points.
     
     '''
 
@@ -199,7 +210,7 @@ def subcutaneous(t_eval, y0, model_input):
         t_span=[t_eval[0], t_eval[-1]],
         y0=y0, t_eval=t_eval
     )
-
+    print(subcutaneous.message)
     return sol_subcutaneous
 
 
@@ -217,14 +228,14 @@ model_input = {
     'k_a': 2.0
 }
 
-fig = plt.figure()
+#fig = plt.figure()
 sol = iv_one_compartment(t_eval, y0, model_input)
 
-
+""" 
 plt.plot(sol.t, sol.y[0, :], label=model_input['name'] + '- q_c')
 plt.plot(sol.t, sol.y[1, :], label=model_input['name'] + '- q_p1')
 
 plt.legend()
 plt.ylabel('drug mass [ng]')
 plt.xlabel('time [h]')
-plt.show()
+plt.show() """
